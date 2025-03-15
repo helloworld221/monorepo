a3a3491ab59f12799f5db1f0d584d5db4d81cba1
skmaiti2050 <skmaiti2050@gmail.com>
2025-03-15 18:14:33 +0530

    fix: typo

--- server/types/express/index.d.ts
+++ server/types/express/index.d.ts
@@ -5,7 +5,7 @@ declare module "express-serve-static-core" {
   interface Request {
     user: any;
     session: any;
-    sessionId: any;
+    sessionID: any;
     isAuthenticated(): boolean;
     logout(callback?: (err?: any) => void): void;
     file?: any;
