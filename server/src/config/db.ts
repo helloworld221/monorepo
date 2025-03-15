61c94c6f764785b75dddf0d23b51861ab43d2d18
skmaiti2050 <skmaiti2050@gmail.com>
2025-03-15 11:34:00 +0530

    fix: check mongo connection issues

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -6,9 +6,13 @@ dotenv.config();
 
 const MONGO_URI = env.MONGO_URI || "mongodb://localhost:27017/media-upload-app";
 
+console.log("MONGO_URI", MONGO_URI);
+
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
   } catch (error) {
     console.error("MongoDB connection error:", error);
     process.exit(1);
